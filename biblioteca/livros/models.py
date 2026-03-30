ad8cb688f7ce76c6986756d7811051d4623d4d5f
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-03-30 17:45:34 -0300

    copiando o projeto do prof

---
+++ biblioteca/livros/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Autor(models.Model): 
+    
+    nome = models.CharField(max_length=100, blank=False, null=False) 
+    
+    nacionalidade = models.CharField(max_length=50, blank=False, null=False)
+    
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+    
+    def __str__(self): 
+        return self.nome 
+
+
+class Livro(models.Model): 
+    
+    titulo = models.CharField(max_length=200, blank=False, null=False) 
+
+    ano_publicacao = models.IntegerField(blank=False, null=False) 
+    
+    autor = models.ForeignKey(Autor, related_name="livros", on_delete=models.CASCADE) 
+    
+    class Meta:
+        verbose_name = 'Livro'
+        verbose_name_plural = 'Livros'
+    
+    def __str__(self): 
+        return self.titulo 
